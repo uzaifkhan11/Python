a06c397a71e6f157af41bbc0631250f6db2c2b77
uzaifkhan11 <135042463+uzaifkhan11@users.noreply.github.com>
2025-08-01 16:54:35 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,7 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "Hello from Flask on Render!"
-
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def home():
+    return 'Hello from Render!'
